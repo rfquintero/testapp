fac4a4dc8fa7abbc805576fd9872dcb18638dd2d
Ruben Quintero <rfquintero@gmail.com>
2017-12-18 00:51:41 -0600

    initial
    - ruben

---
+++ TestApp/Test/Model.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class Model {
+    func isPrime(_ num: Int) -> Bool {
+        return false;
+    }
+    
+    func primes(_ count: Int) -> [Int] {
+        return [];
+    }
+}
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import TestApp
+
+class TestAppTests: XCTestCase {
+    var model: Model!;
+    
+    override func setUp() {
+        super.setUp()
+        model = Model();
+    }
+    
+    
+}
