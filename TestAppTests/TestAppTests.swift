2444c9fc228b644863a982d873bbf58d572702f1
Ruben Quintero <rfquintero@gmail.com>
2017-12-18 00:54:11 -0600

    one test

--- TestAppTests/TestAppTests.swift
+++ TestAppTests/TestAppTests.swift
@@ -9,5 +9,9 @@ class TestAppTests: XCTestCase {
         model = Model();
     }
     
-    
+    func testWhenPrimeIsSentThenReturnTrue() {
+        XCTAssertTrue(model.isPrime(2));
+        XCTAssertTrue(model.isPrime(3));
+        XCTAssertTrue(model.isPrime(5));
+    }
 }
